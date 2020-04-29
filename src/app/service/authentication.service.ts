d501ae95bd66c84263cc546b37a45d5afd261f6f
raveendrau@gmail.com <raveendrau@gmail.com>
2020-04-29 16:13:09 +0530

    changes for basic auth implementation

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,25 +1,44 @@
 import { Injectable } from '@angular/core';
+import {HttpClientService} from './http-client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+   isValidUser:boolean=false;
+
+  constructor(private httpClient:HttpClientService) { }
 
   authenticate(username, password) {
-    if (username === "ruppala" && password === "password") {
-      sessionStorage.setItem('username', username)
-      return true;
-    } else {
-      return false;
-    }
+    this.httpClient.validateUser(username, password).subscribe(response=>this.handleSuccessResponse(response),)
+    // if (username === "ruppala" && password === "password") {
+    //   sessionStorage.setItem('username', username)
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+
+    if (this.isValidUser) {
+        sessionStorage.setItem('username', username)
+      } else {
+      }
+    return this.isValidUser;
+
   }
 
   isUserLoggedIn() {
     let user = sessionStorage.getItem('username')
     console.log(!(user === null))
     return !(user === null)
+  }
+
+  handleSuccessResponse(response){
+    if(response.status=="User validated succesfully"){
+      this.isValidUser=true;
+    }else{
+      this.isValidUser=false;
+    }
   }
 
   logOut() {
--- src/app/service/http-client.service.ts
+++ src/app/service/http-client.service.ts
@@ -1,24 +1,36 @@
 import { Injectable } from '@angular/core';
 import {Employee} from '../model/Employee'
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import { Observable } from 'rxjs';
+import { User } from '../model/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
-
+  username='';
+  password='';
   constructor(private http:HttpClient) { }
   getEmployees():Observable<any>{
-    console.log("Test call..")
-    return this.http.get<Employee[]>('http://localhost:8080/employees')
+    console.log("Test call..");
+    const headers = new HttpHeaders({Authorization:"Basic "+btoa(this.username+":"+this.password)});
+    return this.http.get<Employee[]>('http://localhost:8080/employees',{headers});
   }
   deleteEmployee(employee):Observable<any>{
     console.log("")
-    return this.http.delete('http://localhost:8080/employees/'+employee.empId);
+    const headers = new HttpHeaders({Authorization:"Basic "+btoa(this.username+":"+this.password)});
+    return this.http.delete('http://localhost:8080/employees/'+employee.empId,{headers});
   }
 
   addEmployee(employee):Observable<any>{
-    return this.http.post('http://localhost:8080/employees',employee);
+
+    const headers = new HttpHeaders({Authorization:"Basic "+btoa(this.username+":"+this.password)});
+    return this.http.post('http://localhost:8080/employees',employee,{headers});
+  }
+  validateUser(username,password):Observable<any> {
+    this.username=username;
+    this.password=password;
+    const headers = new HttpHeaders({Authorization:"Basic "+btoa(username+":"+password)});
+    return this.http.get<User>('http://localhost:8080/employees/validateLogin',{headers});
   }
 }
